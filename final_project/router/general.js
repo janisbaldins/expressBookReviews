105dd8837d52030845f7db958b94c2c3d47880b5
Jānis Baldiņš <janis.baldins@gmail.com>
2026-02-14 02:05:37 +0200

    Completed task 11

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,7 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
-
+const axios = require("axios");
 
 
 const doesExist = (username) => {
@@ -46,27 +46,22 @@ public_users.get('/', async function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
 
-// Get book details based on ISBN (Promise)
-public_users.get('/isbn/:isbn', function (req, res) {
-  const isbn = req.params.isbn;
+ const isbn = req.params.isbn;
 
-  new Promise((resolve, reject) => {
-    const book = books[isbn];
-    if (book) resolve(book);
-    else reject("Book not found");
-  })
-    .then((book) => res.status(200).json(book))
-    .catch((err) => res.status(404).json({ message: err }));
-});
-
+  try {
+    const response = await axios.get(`http://localhost:5000/books/isbn/${isbn}`);
+    res.json(response.data);
+  } catch {
+    res.status(404).json({ message: "Book not found" });
+  }
 
 });
 
 
 // Get book details based on author (Promise)
-public_users.get('/author/:author', function (req, res) {
+public_users.get('/author/:author',  function (req, res) {
   const author = req.params.author;
 
   new Promise((resolve, reject) => {
